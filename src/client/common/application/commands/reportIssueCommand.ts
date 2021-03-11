724cd968a4f6434beeb30cc10879371219cf3e09
Paula Camargo <paulitacv25@gmail.com>
2021-03-11 15:22:10 -0500

    Read template from the md file

--- src/client/common/application/commands/reportIssueCommand.ts
+++ src/client/common/application/commands/reportIssueCommand.ts
@@ -4,9 +4,13 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+
 import { inject, injectable } from 'inversify';
 import { IExtensionSingleActivationService } from '../../../activation/types';
 import { ICommandManager } from '../types';
+import { EXTENSION_ROOT_DIR } from '../../../constants';
 
 /**
  * Prompts user to reload VS Code with a custom message, and reloads if necessary.
@@ -19,48 +23,18 @@ export class ReportIssueCommandHandler implements IExtensionSingleActivationServ
         this.commandManager.registerCommand('python.reportIssue', this.openReportIssue, this);
     }
 
+    private templatePath = path.join(EXTENSION_ROOT_DIR, 'resources', 'report_issue_template.md');
+
     public openReportIssue(): void {
+        const templ = this.getIssueTemplate();
         vscode.commands.executeCommand('workbench.action.openIssueReporter', {
             extensionId: 'ms-python.python',
-            issueBody: this.issueTemplate,
+            issueBody: templ,
         });
     }
 
-    private issueTemplate = `## Environment data
-    - VS Code version:
-    - Extension version (available under the Extensions sidebar):
-    - OS and version:
-    - Python version (& distribution if applicable, e.g. Anaconda):
-    - Type of virtual environment used (N/A | venv | virtualenv | conda | ...):
-    - Relevant/affected Python packages and their versions:
-    - Relevant/affected Python-related VS Code extensions and their versions:
-    - Value of the python.languageServer setting:
-
-[**NOTE**: If you suspect that your issue is related to the Microsoft Python Language Server (python.languageServer: 'Microsoft'), please download our new language server [Pylance](https://marketplace.visualstudio.com/items?itemName=ms-python.vscode-pylance) from the VS Code marketplace to see if that fixes your issue]
-
-## Expected behaviour:
-
-## Actual behaviour:
-
-## Steps to reproduce:
-
-[**NOTE**: Self-contained, minimal reproducing code samples are **extremely** helpful and will expedite addressing your issue]
-
-1. XXX
-
-<!--
-Note: If you think a GIF of what is happening would be helpful, consider tools like https://www.cockos.com/licecap/, https://github.com/phw/peek or https://www.screentogif.com/ .
--->
-
-## Logs
-
-<details>
-
-<summary>Output for <code>Python</code> in the <code>Output</code> panel (<code>View</code>→<code>Output</code>, change the drop-down the upper-right of the <code>Output</code> panel to <code>Python</code>)
-</summary>
-
-<p>
-${'``` ```'}
-</p>
-</details>`;
+    public getIssueTemplate(): string {
+        const templ = fs.readFileSync(this.templatePath, 'utf8');
+        return templ;
+    }
 }
