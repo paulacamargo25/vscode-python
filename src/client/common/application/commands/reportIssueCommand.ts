a81a435f4f4807ce0ee7c60d164dc383778d48c4
Paula Camargo <paulitacv25@gmail.com>
2021-05-12 18:47:35 -0500

    Add log output in report issue template

--- src/client/common/application/commands/reportIssueCommand.ts
+++ src/client/common/application/commands/reportIssueCommand.ts
@@ -7,10 +7,11 @@ import * as fs from 'fs-extra';
 import * as path from 'path';
 import { inject, injectable } from 'inversify';
 import { IExtensionSingleActivationService } from '../../../activation/types';
-import { ICommandManager, IDocumentManager, IWorkspaceService } from '../types';
+import { ICommandManager, IWorkspaceService } from '../types';
 import { EXTENSION_ROOT_DIR } from '../../../constants';
 import { IInterpreterService, IInterpreterVersionService } from '../../../interpreter/contracts';
 import { identifyEnvironment } from '../../../pythonEnvironments/common/environmentIdentifier';
+import { getPythonOutputChannelContent } from '../../../logging';
 
 /**
  * Allows the user to report an issue related to the Python extension using our template.
@@ -22,7 +23,6 @@ export class ReportIssueCommandHandler implements IExtensionSingleActivationServ
         @inject(IWorkspaceService) private readonly workspaceService: IWorkspaceService,
         @inject(IInterpreterService) private readonly interpreterService: IInterpreterService,
         @inject(IInterpreterVersionService) private readonly interpreterVersionService: IInterpreterVersionService,
-        @inject(IDocumentManager) private readonly documentManager: IDocumentManager,
     ) {}
 
     public async activate(): Promise<void> {
@@ -31,33 +31,18 @@ export class ReportIssueCommandHandler implements IExtensionSingleActivationServ
 
     private templatePath = path.join(EXTENSION_ROOT_DIR, 'resources', 'report_issue_template.md');
 
-    private async fillTemplateData(): Promise<void> {
-        return new Promise(() => {
-            setTimeout(async () => {
-                const template = await fs.readFile(this.templatePath, 'utf8');
-                const interpreterPath = (await this.interpreterService.getActiveInterpreter())?.path || 'not-selected';
-                const pythonVersion = await this.interpreterVersionService.getVersion(interpreterPath, '');
-                const languageServer =
-                    this.workspaceService.getConfiguration('python').get<string>('languageServer') || 'Not Found';
-                const virtualEnv = await identifyEnvironment(interpreterPath);
-
-                // Assumes caller has shown the Python Output window so that textDocuments is populated with our Log file
-                let pythonLogs = '';
-                const doc = this.documentManager.textDocuments.find((td) => td.languageId === 'Log');
-                if (doc) {
-                    pythonLogs = doc.getText();
-                }
-
-                this.commandManager.executeCommand('workbench.action.openIssueReporter', {
-                    extensionId: 'ms-python.python',
-                    issueBody: template.format(pythonVersion, virtualEnv, languageServer, pythonLogs),
-                });
-            }, 1000);
-        });
-    }
-
     public async openReportIssue(): Promise<void> {
-        await this.commandManager.executeCommand('python.viewOutput');
-        return this.fillTemplateData();
+        const pythonLogs = await getPythonOutputChannelContent();
+        const template = await fs.readFile(this.templatePath, 'utf8');
+        const interpreterPath = (await this.interpreterService.getActiveInterpreter())?.path || 'not-selected';
+        const pythonVersion = await this.interpreterVersionService.getVersion(interpreterPath, '');
+        const languageServer =
+            this.workspaceService.getConfiguration('python').get<string>('languageServer') || 'Not Found';
+        const virtualEnv = await identifyEnvironment(interpreterPath);
+
+        this.commandManager.executeCommand('workbench.action.openIssueReporter', {
+            extensionId: 'ms-python.python',
+            issueBody: template.format(pythonVersion, virtualEnv, languageServer, pythonLogs),
+        });
     }
 }
--- src/test/common/application/commands/reportIssueCommand.unit.test.ts
+++ src/test/common/application/commands/reportIssueCommand.unit.test.ts
@@ -6,14 +6,12 @@
 import * as sinon from 'sinon';
 import * as fs from 'fs-extra';
 import * as path from 'path';
-import * as TypeMoq from 'typemoq';
-import { TextDocument } from 'vscode';
 import { anything, capture, instance, mock, verify, when } from 'ts-mockito';
 import { expect } from 'chai';
 import { LanguageServerType } from '../../../../client/activation/types';
 import { CommandManager } from '../../../../client/common/application/commandManager';
 import { ReportIssueCommandHandler } from '../../../../client/common/application/commands/reportIssueCommand';
-import { ICommandManager, IDocumentManager, IWorkspaceService } from '../../../../client/common/application/types';
+import { ICommandManager, IWorkspaceService } from '../../../../client/common/application/types';
 import { WorkspaceService } from '../../../../client/common/application/workspace';
 import { IInterpreterService, IInterpreterVersionService } from '../../../../client/interpreter/contracts';
 import { InterpreterVersionService } from '../../../../client/interpreter/interpreterVersion';
@@ -22,24 +20,24 @@ import * as EnvIdentifier from '../../../../client/pythonEnvironments/common/env
 import { MockWorkspaceConfiguration } from '../../../startPage/mockWorkspaceConfig';
 import { EXTENSION_ROOT_DIR_FOR_TESTS } from '../../../constants';
 import { InterpreterService } from '../../../../client/interpreter/interpreterService';
-import { DocumentManager } from '../../../../client/common/application/documentManager';
+import * as Logging from '../../../../client/logging/_global';
 
 suite('Report Issue Command', () => {
     let reportIssueCommandHandler: ReportIssueCommandHandler;
     let cmdManager: ICommandManager;
     let workspaceService: IWorkspaceService;
     let interpreterVersionService: IInterpreterVersionService;
-    let documentManager: IDocumentManager;
     let interpreterService: IInterpreterService;
     let identifyEnvironmentStub: sinon.SinonStub;
+    let getPythonOutputContentStub: sinon.SinonStub;
 
     setup(async () => {
         interpreterVersionService = mock(InterpreterVersionService);
         workspaceService = mock(WorkspaceService);
         cmdManager = mock(CommandManager);
         interpreterService = mock(InterpreterService);
-        documentManager = mock(DocumentManager);
 
+        when(cmdManager.executeCommand('workbench.action.openIssueReporter', anything())).thenResolve();
         when(interpreterVersionService.getVersion(anything(), anything())).thenResolve('3.9.0');
         when(workspaceService.getConfiguration('python')).thenReturn(
             new MockWorkspaceConfiguration({
@@ -51,49 +49,44 @@ suite('Report Issue Command', () => {
         identifyEnvironmentStub.resolves(PythonEnvKind.Venv);
 
         cmdManager = mock(CommandManager);
+
+        getPythonOutputContentStub = sinon.stub(Logging, 'getPythonOutputChannelContent');
+        getPythonOutputContentStub.resolves('Python Output');
         reportIssueCommandHandler = new ReportIssueCommandHandler(
             instance(cmdManager),
             instance(workspaceService),
             instance(interpreterService),
             instance(interpreterVersionService),
-            instance(documentManager),
         );
-
-        const document = TypeMoq.Mock.ofType<TextDocument>();
-        document.setup((doc) => doc.getText(TypeMoq.It.isAny())).returns(() => 'Python Output');
-        document.setup((d) => d.languageId).returns(() => 'Log');
-        when(documentManager.textDocuments).thenReturn([document.object]);
-
-        when(cmdManager.executeCommand('python.viewOutput')).thenReturn(Promise.resolve());
-        when(cmdManager.executeCommand('workbench.action.openIssueReporter', anything())).thenResolve();
         await reportIssueCommandHandler.activate();
     });
 
     teardown(() => {
         identifyEnvironmentStub.restore();
+        getPythonOutputContentStub.restore();
     });
 
     test('Test if issue body is filled', async () => {
-        reportIssueCommandHandler.openReportIssue().then(() => {
-            const templatePath = path.join(
-                EXTENSION_ROOT_DIR_FOR_TESTS,
-                'src',
-                'test',
-                'common',
-                'application',
-                'commands',
-                'issueTemplateVenv1.md',
-            );
-            const expectedIssueBody = fs.readFileSync(templatePath, 'utf8');
+        await reportIssueCommandHandler.openReportIssue();
 
-            const args: [string, { extensionId: string; issueBody: string }] = capture<
-                string,
-                { extensionId: string; issueBody: string }
-            >(cmdManager.executeCommand).last();
+        const templatePath = path.join(
+            EXTENSION_ROOT_DIR_FOR_TESTS,
+            'src',
+            'test',
+            'common',
+            'application',
+            'commands',
+            'issueTemplateVenv1.md',
+        );
+        const expectedIssueBody = fs.readFileSync(templatePath, 'utf8');
 
-            verify(cmdManager.registerCommand('python.reportIssue', anything(), anything())).once();
-            verify(cmdManager.executeCommand('workbench.action.openIssueReporter', anything())).once();
-            expect(args[1].issueBody).to.be.equal(expectedIssueBody);
-        });
+        const args: [string, { extensionId: string; issueBody: string }] = capture<
+            string,
+            { extensionId: string; issueBody: string }
+        >(cmdManager.executeCommand).last();
+
+        verify(cmdManager.registerCommand('python.reportIssue', anything(), anything())).once();
+        verify(cmdManager.executeCommand('workbench.action.openIssueReporter', anything())).once();
+        expect(args[1].issueBody).to.be.equal(expectedIssueBody);
     });
 });
