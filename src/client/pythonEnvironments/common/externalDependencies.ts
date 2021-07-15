6d17e99b3780d419711cb75e398a63bec4408d24
Paula Camargo <paulitacv25@gmail.com>
2021-07-15 14:51:21 -0500

    Fix functional tests

--- src/client/pythonEnvironments/common/externalDependencies.ts
+++ src/client/pythonEnvironments/common/externalDependencies.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import * as fsapi from 'fs-extra';
+import fsapi from 'fs-extra';
 import * as path from 'path';
 import * as vscode from 'vscode';
 import { ExecutionResult, ShellOptions, SpawnOptions } from '../../common/process/types';
