1e89b21c07e11ea322cbea579d960384da969540
Paula Camargo <paulitacv25@gmail.com>
2021-12-21 16:23:50 -0500

    Fix unit test pip installer

--- src/client/common/installer/pipInstaller.ts
+++ src/client/common/installer/pipInstaller.ts
@@ -107,7 +107,7 @@ export class PipInstaller extends ModuleInstaller {
         const resource = isResource(info) ? info : undefined;
         const pythonPath = isResource(info) ? undefined : info.path;
         return pythonExecutionFactory
-            .create({ resource, pythonPath, bypassCondaExecution: true })
+            .create({ resource, pythonPath })
             .then((proc) => proc.isModuleInstalled('pip'))
             .catch(() => false);
     }
